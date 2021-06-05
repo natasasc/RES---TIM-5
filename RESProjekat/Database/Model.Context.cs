0ba7ec7d05e1f3d5e37c8395d7b3238292115719
natasasc <scekicnnatasa@gmail.com>
2021-06-05 18:35:45 +0200

    Database izmene
    Izmene u bazi podataka zbog testova.

--- RESProjekat/Database/Model.Context.cs
+++ RESProjekat/Database/Model.Context.cs
@@ -12,7 +12,9 @@ namespace Database
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
-    
+    using System.Diagnostics.CodeAnalysis;
+
+    [ExcludeFromCodeCoverage]
     public partial class sqlDBEntities : DbContext
     {
         public sqlDBEntities()
