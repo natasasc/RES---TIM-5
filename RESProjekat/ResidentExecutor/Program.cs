8c8f803e835f71649843b5fcc56f2241b70b5516
natasasc <scekicnnatasa@gmail.com>
2021-06-05 18:36:24 +0200

    RE implementacija
    Izmene u Resident Executor komponenti zbog testova.

--- RESProjekat/ResidentExecutor/Program.cs
+++ RESProjekat/ResidentExecutor/Program.cs
@@ -6,30 +6,113 @@ using System.Threading.Tasks;
 using System.Xml;
 using System.IO;
 using CalculationFunction;
+using Common;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 
 namespace ResidentExecutor
 {
-    class Program
+    public class Program
     {
+        private static readonly IExecute f = new CalculationFunction.Functions();
+
+        [ExcludeFromCodeCoverage]
         static void Main(string[] args)
         {
-            XmlDocument xmlDoc = new XmlDocument();
             string xmlPath = Directory.GetCurrentDirectory() + @"\data.xml";
-            xmlDoc.Load(xmlPath);
+            XmlDocument xmlDoc = ReadXML(xmlPath);
+            string id = "id";
+            string time = "time";
+            List<XmlNodeList> lista = LoadNodes(xmlDoc, id, time);
 
-            XmlNodeList list = xmlDoc.GetElementsByTagName("id");
-            XmlNodeList time = xmlDoc.GetElementsByTagName("time");
-            int seconds = int.Parse(time[0].InnerText);
+            List<string> podaci = Validate(lista);
+            Work(podaci);
+
+            //Console.ReadLine();
+        }
+
+        public static XmlDocument ReadXML(string path)
+        {
+            if (path == null)
+            {
+                throw new ArgumentNullException("Naziv fajla ne moze biti null.");
+            }
+            if (path.Trim() =="")
+            {
+                throw new ArgumentException("Naziv fajla ne moze biti prazan.");
+            }
+
+            List<XmlNodeList> ret = new List<XmlNodeList>();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(path);
+
+            return xmlDoc;
+        }
+
+        public static List<XmlNodeList> LoadNodes(XmlDocument xmlDoc, string id, string time)
+        {
+            List<XmlNodeList> ret = new List<XmlNodeList>();
+
+            XmlNodeList list = xmlDoc.GetElementsByTagName(id);
+            if (list.Count == 0)
+                throw new ArgumentException("Nije pronadjen element sa unetim nazivom.");
+
+            XmlNodeList list2 = xmlDoc.GetElementsByTagName(time);
+            if (list2.Count == 0)
+                throw new ArgumentException("Nije pronadjen element sa unetim nazivom.");
+
+            ret.Add(list2);
+            ret.Add(list);
+
+            return ret;
+        }
+
+        public static List<string> Validate(List<XmlNodeList> lista)
+        {
+            if (lista == null)
+                throw new ArgumentNullException("Lista ne moze biti null.");
+            if (lista.Count != 2)
+                throw new ArgumentException("Lista mora da sadrzi 2 elementa.");
+
+            List<string> podaci = new List<string>();
+            podaci.Add(lista[0][0].InnerText.ToString());
+            for (int i = 0; i < lista[1].Count; i++)
+            {
+                podaci.Add(lista[1][i].InnerText.ToString());
+            }
+
+            return podaci;
+        }
+
+        public static void Work(List<string> podaci)
+        {
+            foreach (var item in podaci)
+            { 
+                if (item == null)
+                    throw new ArgumentNullException("Podaci ne smeju biti null.");
+            }
+
+            for (int i = 1; i < podaci.Count; i++)
+            {
+                if (podaci[i] != "1" && podaci[i] != "2" && podaci[i] != "3")
+                    throw new ArgumentException("Identifikacije funkcija mogu biti 1, 2 ili 3");
+            }
+
+            Regex reg = new Regex("^[0-9]+$");
+            if (!reg.IsMatch(podaci[0]))
+                throw new FormatException("Vreme nije u dobrom formatu.");
+
+            int seconds = int.Parse(podaci[0]);
 
             Console.Write("Broj sekundi izmedju poziva funkcija: " + seconds.ToString() + "\n");
 
             double value;
             while (true)
             {
-                for (int i = 0; i < list.Count; i++)
+                for (int i = 1; i < podaci.Count; i++)
                 {
-                    Console.WriteLine("Poziv funkcije " + list[i].InnerText.ToString());
-                    value = Functions.IdAcceptance(int.Parse(list[i].InnerText));
+                    Console.WriteLine("Poziv funkcije " + podaci[i]);
+                    value = f.IdAcceptance(int.Parse(podaci[i]));
                     if (value != -1)
                     {
                         WriteData(value);
@@ -37,15 +120,17 @@ namespace ResidentExecutor
                     System.Threading.Thread.Sleep(seconds * 1000);
                 }
             }
-
-            //Console.ReadLine();
         }
 
-        private static void WriteData(double value)
+        public static void WriteData(double value)
         {
+            if (value < 0)
+            {
+                throw new ArgumentException("Vrednost ne sme biti manja od 0.");
+            }
+
             string xmlPath = Directory.GetCurrentDirectory() + @"\funcData.xml";
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(xmlPath);
+            XmlDocument xmlDoc = ReadXML(xmlPath);
 
             XmlNode potrosnja = xmlDoc.CreateElement("usage");
             potrosnja.InnerText = value.ToString();
